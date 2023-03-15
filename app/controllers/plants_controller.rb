9b951263740c8778f3f63caa7a2c436608f57706
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-15 13:15:46 +0300

    Completed task

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,21 +1,44 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
-    plants = Plant.all
-    render json: plants
+    @plants = Plant.all
+    render json: @plants
   end
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    @plant = Plant.find(params[:id])
+    render json: @plant
   end
 
   # POST /plants
   def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
+    @plant = Plant.new(plant_params)
+
+    if @plant.save
+      render json: @plant, status: :created
+    else
+      render json: @plant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /plants/:id
+  def update
+    @plant = Plant.find(params[:id])
+
+    if @plant.update(plant_params)
+      render json: @plant
+    else
+      render json: @plant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+
+    head :no_content
   end
 
   private
@@ -24,3 +47,4 @@ class PlantsController < ApplicationController
     params.permit(:name, :image, :price, :is_in_stock)
   end
 end
+
